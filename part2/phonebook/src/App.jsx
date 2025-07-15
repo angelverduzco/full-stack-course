87febebbaa938e196aaa8393863b7c2e7fa7604a
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-07-14 20:11:01 -0600

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({onChange}) => {
   return (
@@ -39,15 +41,19 @@ const PersonForm = ({
 const Persons = ({persons}) => persons.map((person) => <p key={person.id}> {person.name} {person.number} </p>)
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const persons = response.data
+        setPersons(persons)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
